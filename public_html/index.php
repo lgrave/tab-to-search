15a5dbd0274d1f8af96b95897921c7936f268cc6
Luis Grave <lgrave@kuantokusta.com>
2014-08-07 01:34:53 +0100

    some output

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,7 @@
 	<link type="application/opensearchdescription+xml" rel="search" href="/osdd.xml" />
 </head>
 <body>
+	<h1>Welcome!</h1>
 	<?php if (isset($_GET['q'])) : ?>
 		Query: <?= htmlentities($_GET['q']) ?>
 	<?php endif; ?>
